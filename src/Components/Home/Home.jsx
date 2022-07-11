00893ab3693ebee69f77c6d16f58fba3737e3f93
Mwita Chacha <jonahhchacha@gmail.com>
2022-07-12 00:11:54 +0300

    Consuming Bored API using axios

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,21 +1,34 @@
 import React, { useState, useEffect } from "react";
 import { HashLoader } from "react-spinners";
 import "./Home.css";
-import Ape from "../../assets/bored-ape.png"
-
+import Ape from "../../assets/bored-ape.png";
+import { motion } from "framer-motion";
+import axios from "axios";
 const Home = () => {
   const [loading, setLoading] = useState(true);
+  const [activity, setActivity] = useState([]);
 
   useEffect(() => {
     setInterval(() => {
       setLoading(false);
-    }, 500000);
+    }, 5000);
   });
+
+  useEffect(() => {
+    axios.get("https://www.boredapi.com/api/activity").then((response) => {
+      console.log(response.data);
+      setActivity(response.data);
+    });
+  }, []);
 
   if (loading) {
     return (
       <div>
-        <div className="welcome w-1/2 m-auto p-20 mt-20 rounded-lg">
+        <motion.div
+          animate={{ y: 10 }}
+          transition={{ ease: "easeOut", duration: 2 }}
+          className="welcome w-1/2 m-auto p-20 mt-20 rounded-lg"
+        >
           <div className="flex items-center justify-center">
             <h1 className="text-2xl md:text-6xl font-bold mr-5">BORED?</h1>
             <HashLoader
@@ -24,8 +37,12 @@ const Home = () => {
               color="white"
             />
           </div>
-          <img className="md:w-1/2 mx-auto animate-pulse duration-1000" src={Ape} alt="" />
-        </div>
+          <img
+            className="md:w-1/2 mx-auto animate-pulse duration-1000"
+            src={Ape}
+            alt=""
+          />
+        </motion.div>
       </div>
     );
   } else {
